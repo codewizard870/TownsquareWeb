3edc09783c9ecfc083acfd62a57a9d4f1137c927
Helme0315 <nakamurashota0315@gmail.com>
2023-07-25 21:17:43 +0900

    Fix overflow content

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -34,7 +34,7 @@ function Features() {
   }, []);
 
   return (
-    <div className="features component bg-black hidden md:block lg:block" id="feature" data-component="Features">
+    <div className="features component bg-black hidden md:block lg:block overflow-hidden" id="feature" data-component="Features">
       <Controller>
         <Scene duration={5000} pin offset={`${offset}px`}>
           <div className="w-full relative">
