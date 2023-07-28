c05dc192ed93d27848082215de1feab0a259c418
bluecat870 <alenzer0902@gmail.com>
2023-07-28 12:45:53 -0400

    fix feature1 first position

--- src/components/Features/screen1.tsx
+++ src/components/Features/screen1.tsx
@@ -16,7 +16,7 @@ function Screen1() {
         if (ref1.current != null && (ref1.current as any).style != null) {
           let node = ref1.current as any;
           node.style.opacity = 1 - self.progress;
-          node.style.transform = `translate(-50%, ${-20 * self.progress}vh)`;
+          node.style.transform = `translate(-50%, ${-45 * self.progress}vh)`;
         }
       },
     });
@@ -27,7 +27,7 @@ function Screen1() {
       onUpdate: (self) => {
         if (ref2.current != null && (ref2.current as any).style != null) {
           let node = ref2.current as any;
-          node.style.transform = `translate(calc(-50% - ${20 * self.progress}vw), calc(-50% - ${10 * self.progress}vh))`
+          node.style.transform = `translate(calc(-50% - ${20 * self.progress}vw), calc(-50% - ${30 * self.progress}vh))`
         }
       },
     });
@@ -39,7 +39,7 @@ function Screen1() {
         if (ref3.current != null && (ref3.current as any).style != null) {
           let node = ref3.current as any;
           node.style.opacity = self.progress;
-          node.style.transform = `translate(calc(-50% + ${50 * self.progress}% - ${50 * self.progress}vw), -50%)`
+          node.style.transform = `translate(calc(-50% - ${40 * self.progress}vw), -50%)`
         }
       },
     });
@@ -52,7 +52,7 @@ function Screen1() {
         if (ref2.current != null && (ref2.current as any).style != null) {
           let node = ref2.current as any;
           node.style.opacity = 1 - self.progress;
-          node.style.transform = `translate(calc(-50% - 20vw - ${50 * self.progress}vw), calc(-50% - 10vh + ${50 * self.progress}vh))`
+          node.style.transform = `translate(calc(-50% - 20vw - ${50 * self.progress}vw), calc(-50% - 30vh + ${50 * self.progress}vh))`
         }
       },
     });
@@ -64,7 +64,7 @@ function Screen1() {
         if (ref3.current != null && (ref3.current as any).style != null) {
           let node = ref3.current as any;
           node.style.opacity = 1 - self.progress;
-          node.style.transform = `translate(calc(-50vw - ${50 * self.progress}vw), calc(-50% + ${50 * self.progress}vh))`
+          node.style.transform = `translate(calc(-50% - 40vw - ${50 * self.progress}vw), calc(-50% + ${50 * self.progress}vh))`
         }
       },
     });
@@ -73,7 +73,9 @@ function Screen1() {
 
   return (
     <>
-      <div ref={ref1} className="absolute w-[340px] flex flex-col left-1/2 -translate-x-1/2 top-0">
+      <div 
+        ref={ref1} className="absolute w-[340px] flex flex-col left-1/2 -translate-x-1/2 top-[25vh]"
+      >
         <h1 className="text-3xl  font-semibold text-center">Introducing</h1>
         <div className="flex gap-4 self-center items-center">
           <img src={images.logo} alt="logo" className="w-12 h-12" />
@@ -82,7 +84,7 @@ function Screen1() {
         <img src="/images/financial_img.png" alt="sign" className="self-center" />
       </div>
       <img ref={ref2} src="/images/features/feed.svg" alt='feature1' className="absolute w-[300px] self-center  -translate-x-1/2 -translate-y-1/2"
-        style={{ left: `50vw`, top: `60vh` }}
+        style={{ left: `50vw`, top: `80vh` }}
       />
       <div ref={ref3} className="absolute w-[500px] opacity-0 -translate-x-1/2 -translate-y-1/2"
         style={{ left: `100vw`, top: `50vh` }}
