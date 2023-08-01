ea5715c44807fa30ccb315989db02b1d55807b5d
d2Dreamer <41088824+d2Dreamer@users.noreply.github.com>
2023-08-01 16:32:13 +0100

    Updated Cursor

--- src/components/RightPanel/index.tsx
+++ src/components/RightPanel/index.tsx
@@ -15,10 +15,10 @@ const RightPanelMenu = (props: Stage) => {
     <div className={`right-panel-menu fixed right-[-20px] top-1/3 z-50 font-Outfit font-bold text-sm text-[#7F7F7F] transform transition-transform -translate-x-20 ${props.isVisible ? '-translate-x-9' : 'translate-x-full'
       }`}>
       <ul className='space-y-4 list-disc'>
-        <li className={props.stage === 'Introduction' ? 'active' : ''} onClick={() => handlePanelClick("homeDiv")}>Introduction</li>
-        <li className={props.stage === 'Vision' ? 'active' : ''} onClick={() => handlePanelClick("visionDiv")}>Vision</li>
-        <li className={props.stage === 'Features' ? 'active' : ''} onClick={() => handlePanelClick("feature")}>Features</li>
-        <li className={props.stage === 'Early' ? 'active' : ''} onClick={() => handlePanelClick("earlyAccessDiv")}>Get early access</li>
+        <li className={props.stage === 'Introduction' ? 'active' : 'cursor-pointer'} onClick={() => handlePanelClick("homeDiv")}>Introduction</li>
+        <li className={props.stage === 'Vision' ? 'active' : 'cursor-pointer'} onClick={() => handlePanelClick("visionDiv")}>Vision</li>
+        <li className={props.stage === 'Features' ? 'active' : 'cursor-pointer'} onClick={() => handlePanelClick("feature")}>Features</li>
+        <li className={props.stage === 'Early' ? 'active' : 'cursor-pointer'} onClick={() => handlePanelClick("earlyAccessDiv")}>Get early access</li>
       </ul>
     </div>
   );
