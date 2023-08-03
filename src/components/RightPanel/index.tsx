3f9656a7529a5c08f374edbd3ffd025f86f7ef78
Pelumi <fatolupelumi@gmail.com>
2023-08-03 23:37:43 +0100

    right-side-panel-mobile

--- src/components/RightPanel/index.tsx
+++ src/components/RightPanel/index.tsx
@@ -1,24 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 interface Stage {
   stage: any;
   isVisible: boolean;
 }
 
 const RightPanelMenu = (props: Stage) => {
-
   const handlePanelClick = (title: string) => {
-      document.getElementById(title)!.scrollIntoView({behavior: 'smooth', block: "start", inline: "nearest"});
+    document
+      .getElementById(title)!
+      .scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest",
+      });
   };
-  
 
   return (
-    <div className={`right-panel-menu fixed right-[-20px] top-1/3 z-50 font-Outfit font-bold text-sm text-[#7F7F7F] transform transition-transform -translate-x-20 ${props.isVisible ? '-translate-x-9' : 'translate-x-full'
-      }`}>
-      <ul className='space-y-2 list-disc cursor-default'>
-        <li className={props.stage === 'Introduction' ? 'active' : 'cursor-pointer'} onClick={() => handlePanelClick("homeDiv")}>Introduction</li>
-        <li className={props.stage === 'Vision' ? 'active' : 'cursor-pointer'} onClick={() => handlePanelClick("visionDiv")}>Vision</li>
-        <li className={props.stage === 'Features' ? 'active' : 'cursor-pointer'} onClick={() => handlePanelClick("feature")}>Features</li>
-        <li className={props.stage === 'Early' ? 'active' : 'cursor-pointer'} onClick={() => handlePanelClick("earlyAccessDiv")}>Get early access</li>
+    <div
+      className={`right-panel-menu border-t-2 border-white w-[110%] px-22 md:border-none md:w-fit fixed bottom-0 right-[-7.5rem] md:right-[-20px] md:top-1/3 z-50 font-Outfit font-bold text-sm text-[#7F7F7F] transform transition-transform -translate-x-20 ${
+        props.stage === "Early" && props.isVisible
+          ? "-translate-x-[8.5rem] md:-translate-x-9"
+          : props.isVisible
+          ? "-translate-x-9"
+          : "translate-x-full"
+      }`}
+    >
+      <div className="backdrop-brightness-50 bg-white/30">
+
+      </div>
+      <ul className="flex items-center justify-between space-x-2 list-disc cursor-default md:flex-col md:space-y-2">
+        <li
+          className={
+            props.stage === "Introduction" ? "active" : "cursor-pointer"
+          }
+          onClick={() => handlePanelClick("homeDiv")}
+        >
+          Introduction
+        </li>
+        <li
+          className={props.stage === "Vision" ? "active" : "cursor-pointer"}
+          onClick={() => handlePanelClick("visionDiv")}
+        >
+          Vision
+        </li>
+        <li
+          className={props.stage === "Features" ? "active" : "cursor-pointer"}
+          onClick={() => handlePanelClick("feature")}
+        >
+          Features
+        </li>
+        <li
+          className={props.stage === "Early" ? "active" : "cursor-pointer"}
+          onClick={() => handlePanelClick("earlyAccessDiv")}
+        >
+          Get early access
+        </li>
       </ul>
     </div>
   );
