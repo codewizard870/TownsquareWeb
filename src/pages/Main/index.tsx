dee7fad8e53833d7987af7817a3203cb9b405047
codewizard870 <alenzer0902@gmail.com>
2023-08-11 13:29:38 -0400

    remove desktop features

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -10,8 +10,8 @@ const Features = lazy(() => import("../../components/Features"))
 
 function Main() {
   const [currentComponent, setCurrentComponent] = useState<string | null>(null);
-  const components = ['Home', 'Introduction', 'Vision', 'Features','FeaturesMobile','Early'];
-  const [isTransparencyVisible, setIsTransparencyVisible] =  useState(false);
+  const components = ['Home', 'Introduction', 'Vision', 'Features', 'FeaturesMobile', 'Early'];
+  const [isTransparencyVisible, setIsTransparencyVisible] = useState(false);
   const [isJoinAppear, setIsJoinAppear] = useState(false);
 
   useEffect(() => {
@@ -34,11 +34,11 @@ function Main() {
         return null;
       });
       const visibleComponent = positions.find((position) => {
-        if(position && position?.name == "Introduction") {
-          return position && (position.top <= scrollY + windowHeight / 2 && position.bottom >= scrollY + windowHeight / 2) && position.right <= windowWidth && position.right >= windowWidth/3;
-        } else if(position && position?.name == "Vision") {
-          return position && (position.top <= scrollY + windowHeight / 2 && position.bottom >= scrollY + windowHeight / 2) && (position.right > windowWidth / 3 || position.right > windowWidth*-3 );
-        } else if(position) {
+        if (position && position?.name == "Introduction") {
+          return position && (position.top <= scrollY + windowHeight / 2 && position.bottom >= scrollY + windowHeight / 2) && position.right <= windowWidth && position.right >= windowWidth / 3;
+        } else if (position && position?.name == "Vision") {
+          return position && (position.top <= scrollY + windowHeight / 2 && position.bottom >= scrollY + windowHeight / 2) && (position.right > windowWidth / 3 || position.right > windowWidth * -3);
+        } else if (position) {
           return position && position.top <= scrollY + windowHeight / 2 && position.bottom >= scrollY + windowHeight / 2;
         }
       });
@@ -63,20 +63,20 @@ function Main() {
 
   return (
     <div>
-      <Home clickWaitlist={handleClick}/>
-       <Transparency
+      <Home clickWaitlist={handleClick} />
+      <Transparency
         isJoinAppear={isJoinAppear}
         isVisible={isTransparencyVisible}
         onClose={handleClick}
       />
       <RightPanelMenu stage={currentComponent} isVisible={currentComponent !== "Home" && currentComponent !== null} />
-      <div className={`${currentComponent !== "Home" && currentComponent !== null ? "fixed bottom-0 z-40 backdrop-blur-xl bg-white/30 h-[44px] w-[100%] md:hidden": "hidden"}`}>
-        
+      <div className={`${currentComponent !== "Home" && currentComponent !== null ? "fixed bottom-0 z-40 backdrop-blur-xl bg-white/30 h-[44px] w-[100%] md:hidden" : "hidden"}`}>
+
       </div>
       <Vision />
-     <Suspense fallback={<div>LOADING</div>}>
-       <Features />
-     </Suspense>
+      {/* <Suspense fallback={<div>LOADING</div>}>
+        <Features />
+      </Suspense> */}
       <FeaturesMobile />
       <EarlyAccess />
     </div>
